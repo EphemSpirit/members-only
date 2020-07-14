ec2ca4b47e67bd4467fb384a85db3a2d81596b1a
Drew Hund <languageinvestigator@gmail.com>
2020-07-14 19:43:58 -0400

    made form for new posts

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -9,8 +9,20 @@ class PostsController < ApplicationController
     @post = Post.new
   end
 
+  def create
+    @post = Post.new(post_params)
+
+    if @post.save
+      redirect_to root_path
+    else
+      render :new
+    end
+
+  end
+
   private
 
   def post_params
-    params.require(:post).permit(:title, :body)
+    params.require(:post).permit(:title, :body, :author)
+  end
 end
