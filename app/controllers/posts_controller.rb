5140b60070a3ebbbf8a6978872f60379a747ddcf
Drew Hund <languageinvestigator@gmail.com>
2020-07-14 19:21:40 -0400

    basic model setup

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,2 +1,16 @@
 class PostsController < ApplicationController
+  before_action :authenticate_user!, except: [:index]
+
+  def index
+    @posts = Post.all
+  end
+
+  def new
+    @post = Post.new
+  end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:title, :body)
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_07_14_224853) do
+ActiveRecord::Schema.define(version: 2020_07_14_230902) do
 
   create_table "posts", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "title"
+    t.text "body"
+    t.string "author"
   end
 
   create_table "users", force: :cascade do |t|
