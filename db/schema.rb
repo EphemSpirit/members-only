1ef07e5d6ac80aa3650ad228fb10557aa899e1e5
Drew Hund <languageinvestigator@gmail.com>
2020-07-15 18:55:53 -0400

    fixed form for Posts#new

--- db/schema.rb
+++ db/schema.rb
@@ -10,14 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_07_15_205430) do
+ActiveRecord::Schema.define(version: 2020_07_15_213544) do
 
   create_table "posts", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "title"
     t.text "body"
-    t.string "author"
     t.integer "user_id"
   end
 
