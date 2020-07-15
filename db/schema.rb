37c3e9ad5cd632c9952fa73905b1383696d45eab
Drew Hund <languageinvestigator@gmail.com>
2020-07-15 17:19:49 -0400

    added user account functionality

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_07_14_230902) do
+ActiveRecord::Schema.define(version: 2020_07_15_205430) do
 
   create_table "posts", force: :cascade do |t|
     t.datetime "created_at", null: false
@@ -18,6 +18,7 @@ ActiveRecord::Schema.define(version: 2020_07_14_230902) do
     t.string "title"
     t.text "body"
     t.string "author"
+    t.integer "user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -28,8 +29,11 @@ ActiveRecord::Schema.define(version: 2020_07_14_230902) do
     t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "name"
+    t.string "username"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+    t.index ["username"], name: "index_users_on_username", unique: true
   end
 
 end
